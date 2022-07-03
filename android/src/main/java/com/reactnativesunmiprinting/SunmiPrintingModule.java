edf43d5a0d4636650e605345c848473f8d961e1d
Sascha1251 <slavisa.markovic@web.de>
2022-07-03 22:37:34 +0200

    Add methods for underline printing; Apply prettier

--- android/src/main/java/com/reactnativesunmiprinting/SunmiPrintingModule.java
+++ android/src/main/java/com/reactnativesunmiprinting/SunmiPrintingModule.java
@@ -3,9 +3,7 @@ package com.reactnativesunmiprinting;
 import android.graphics.Bitmap;
 import android.os.RemoteException;
 import android.util.Base64;
-
 import androidx.annotation.NonNull;
-
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -14,12 +12,13 @@ import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = SunmiPrintingModule.NAME)
 public class SunmiPrintingModule extends ReactContextBaseJavaModule {
+
     public static final String NAME = "SunmiPrinting";
     public static final String SERVICE_NOT_FOUND = "SERVICE_NOT_FOUND";
 
     public SunmiPrintingModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-      SunmiPrintHelper.getInstance().initSunmiPrinterService(reactContext);
+        super(reactContext);
+        SunmiPrintHelper.getInstance().initSunmiPrinterService(reactContext);
     }
 
     @Override
@@ -28,121 +27,166 @@ public class SunmiPrintingModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
-  @ReactMethod
-  public void print(String text, Promise promise){
-    SunmiPrintHelper.getInstance().printText2(text);
-    promise.resolve(true);
-  }
-
-  @ReactMethod
-  public void clearBuffer(Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
+    @ReactMethod
+    public void print(String text, Promise promise) {
+        SunmiPrintHelper.getInstance().printText2(text);
+        promise.resolve(true);
     }
 
-    try {
-      SunmiPrintHelper.getInstance().sunmiPrinterService.clearBuffer();
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
-    }
-  }
+    @ReactMethod
+    public void clearBuffer(Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
 
-  @ReactMethod
-  public void printOriginalText(String text, Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
-    }
-
-    try {
-      SunmiPrintHelper.getInstance().sunmiPrinterService.printOriginalText(text, null);
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
-    }
-  }
-
-  @ReactMethod
-  public void setAlignment(int alignment, Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.clearBuffer();
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
     }
 
-    try {
-      SunmiPrintHelper.getInstance().sunmiPrinterService.setAlignment(alignment, null);
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
-    }
-  }
+    @ReactMethod
+    public void printOriginalText(String text, Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
 
-  @ReactMethod
-  public void setFontSize(float fontSize, Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
-    }
-
-    try {
-      SunmiPrintHelper.getInstance().sunmiPrinterService.setFontSize(fontSize, null);
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
-    }
-  }
-
-  @ReactMethod
-  public void lineWrap(int linesToWrap, Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.printOriginalText(text, null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
     }
 
-    try {
-      SunmiPrintHelper.getInstance().sunmiPrinterService.lineWrap(linesToWrap, null);
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
-    }
-  }
+    @ReactMethod
+    public void setAlignment(int alignment, Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
 
-  @ReactMethod
-  public void printBitmap(String base64, int height, int width, Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
-    }
-
-    try {
-      byte[] decoded = Base64.decode(base64, Base64.DEFAULT);
-      final Bitmap bitMap = BitmapUtil.decodeBitmap(decoded, width, height);
-      SunmiPrintHelper.getInstance().sunmiPrinterService.printBitmap(bitMap, null);
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
-    }
-    promise.resolve(true);
-  }
-
-  @ReactMethod
-  public void setBold(boolean bold, Promise promise){
-    if(SunmiPrintHelper.getInstance().sunmiPrinterService == null){
-      promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
-      return;
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.setAlignment(alignment, null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
     }
 
-    try {
-      if (bold) {
-        SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.boldOn(), null);
-      } else {
-        SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.boldOff(), null);
-      }
-      promise.resolve(true);
-    } catch(RemoteException e) {
-      promise.reject(e);
+    @ReactMethod
+    public void setFontSize(float fontSize, Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.setFontSize(fontSize, null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
     }
-  }
+
+    @ReactMethod
+    public void lineWrap(int linesToWrap, Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.lineWrap(linesToWrap, null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void printBitmap(String base64, int height, int width, Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            byte[] decoded = Base64.decode(base64, Base64.DEFAULT);
+            final Bitmap bitMap = BitmapUtil.decodeBitmap(decoded, width, height);
+            SunmiPrintHelper.getInstance().sunmiPrinterService.printBitmap(bitMap, null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void setBold(boolean bold, Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            if (bold) {
+                SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.boldOn(), null);
+            } else {
+                SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.boldOff(), null);
+            }
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void underlineWithOneDotWidthOn(Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.underlineWithOneDotWidthOn(), null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void underlineWithTwoDotWidthOn(Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.underlineWithTwoDotWidthOn(), null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void underlineOff(Promise promise) {
+        if (SunmiPrintHelper.getInstance().sunmiPrinterService == null) {
+            promise.reject(SERVICE_NOT_FOUND, "Printer Service is null");
+            return;
+        }
+
+        try {
+            SunmiPrintHelper.getInstance().sunmiPrinterService.sendRAWData(ESCUtil.underlineOff(), null);
+            promise.resolve(true);
+        } catch (RemoteException e) {
+            promise.reject(e);
+        }
+    }
 }
--- android/src/main/java/com/reactnativesunmiprinting/SunmiPrintingPackage.java
+++ android/src/main/java/com/reactnativesunmiprinting/SunmiPrintingPackage.java
@@ -1,17 +1,16 @@
 package com.reactnativesunmiprinting;
 
 import androidx.annotation.NonNull;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SunmiPrintingPackage implements ReactPackage {
+
     @NonNull
     @Override
     public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
